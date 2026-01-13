cbf595449fc7b9eaa515d89de9af0ddc604f7be8
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2026-01-12 22:40:38 -0800

    Update install-new-machine.ps1

--- fire/install-new-machine.ps1
+++ fire/install-new-machine.ps1
@@ -4,10 +4,6 @@
 # Set-ExecutionPolicy RemoteSigned
 
 Set-WindowsExplorerOptions -EnableShowFileExtensions
-$Boxstarter.RebootOk=$true # Allow reboots?
-$Boxstarter.NoPassword=$false # Is this a machine with no login password?
-$Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
-
 choco install vscode
 
 winget install HiBitSoftware.StartUpManager
