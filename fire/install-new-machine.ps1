ce58ce4a4eed9de7d706b4501f538498f5539936
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2026-01-12 22:33:47 -0800

    Add HiBit Startup Manager installation to script

--- fire/install-new-machine.ps1
+++ fire/install-new-machine.ps1
@@ -8,7 +8,10 @@ $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
+winget install HiBitSoftware.StartUpManager
+
 choco install gh
+
 choco install clink
 # Correct time is important
 choco install nettime
