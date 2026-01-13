1567750e06643bc90e5aaddc170389ceb1d980fc
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2026-01-12 22:29:39 -0800

    Update install-new-machine.ps1

--- fire/install-new-machine.ps1
+++ fire/install-new-machine.ps1
@@ -8,6 +8,7 @@ $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
+choco install gh
 choco install clink
 # Correct time is important
 choco install nettime
