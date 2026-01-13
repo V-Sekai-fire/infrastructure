559ac7d2aad29137818c44745a43e999e20ad6ba
K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>
2026-01-12 22:38:26 -0800

    Update install-new-machine.ps1

--- fire/install-new-machine.ps1
+++ fire/install-new-machine.ps1
@@ -8,6 +8,8 @@ $Boxstarter.RebootOk=$true # Allow reboots?
 $Boxstarter.NoPassword=$false # Is this a machine with no login password?
 $Boxstarter.AutoLogin=$true # Save my password securely and auto-login after a reboot
 
+choco install vscode
+
 winget install HiBitSoftware.StartUpManager
 
 choco install gh
